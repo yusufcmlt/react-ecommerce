3d514f208e8d9a2f365b9fa95329c0d0d37dffaf
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-10-08 23:03:19 +0300

    break homepage into smaller components

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Directory from "../../components/directory/Directory";
+
+import "./HomePage-style.scss";
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
