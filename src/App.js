483d5ea013028e4b0987432e021b6d137cf38d58
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-10-09 16:49:48 +0300

    set up header

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/Homepage";
 import ShopPage from "./pages/shop/Shop";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
