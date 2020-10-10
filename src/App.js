d5baae42ecaafa5809586271336f23c89c1e29b2
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-10-10 16:38:02 +0300

    implement firebase authentication

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from "./pages/homepage/Homepage";
 import ShopPage from "./pages/shop/Shop";
 import Header from "./components/header/Header";
 import SignInOut from "./pages/signinout-page/SignInOut";
-import { auth } from "./firebase/firebase-utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase-utils";
 
 export default class App extends React.Component {
   constructor() {
@@ -20,12 +20,21 @@ export default class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+          console.log(this.state);
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
-    console.log(this.unsubscribeFromAuth);
   }
 
   componentWillUnmount() {
--- src/pages/signinout-page/SignInOut.jsx
+++ src/pages/signinout-page/SignInOut.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import SignIn from "../../components/sign-in/SignIn";
+import SignUp from "../../components/sign-up/SignUp";
 
 import "./SignInOut-style.scss";
 
@@ -8,6 +9,7 @@ export default function SignInOut() {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 }
