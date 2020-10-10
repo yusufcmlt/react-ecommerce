872908d50d2ea88d7c03eb5d64f7525bef155341
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-10-10 16:46:53 +0300

    implement sign in function

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ export default class App extends React.Component {
               ...snapShot.data(),
             },
           });
-          console.log(this.state);
+          //console.log(this.state);
         });
       }
       this.setState({ currentUser: userAuth });
--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import FormInput from "../form-input/FormInput";
 import CustomButton from "../../components/custom-button/CustomButton";
 
-import { signInWithGoogle } from "../../firebase/firebase-utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase-utils";
 
 import "./SignIn-style.scss";
 
@@ -16,9 +16,17 @@ export default class SignIn extends Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
   };
   handleChange = (event) => {
     const { value, name } = event.target;
