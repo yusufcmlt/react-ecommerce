70e9b691f3deb823bb9dfd3ad3c48309b8ba2532
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-10-10 16:53:11 +0300

    change sign in button

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -57,7 +57,11 @@ export default class SignIn extends Component {
           />
           <div className="buttons">
             <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn={true}>
+            <CustomButton
+              type="button"
+              onClick={signInWithGoogle}
+              isGoogleSignIn={true}
+            >
               Sign in with Google
             </CustomButton>
           </div>
