84d1f8438bf50a86a8a873bf127edbad052e8e78
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-10-14 12:32:21 +0300

    create category page

--- src/components/collections-overview/CollectionsOverview.jsx
+++ src/components/collections-overview/CollectionsOverview.jsx
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import CollectionPreview from "../collection-preview/CollectionPreview";
-import { selectCollections } from "../../redux/shop/shop-selectors.js";
+import { selectCollectionsForPreview } from "../../redux/shop/shop-selectors.js";
 
 const CollectionsOverview = ({ collections }) => {
   return (
@@ -16,7 +16,7 @@ const CollectionsOverview = ({ collections }) => {
 };
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 import CollectionsOverview from "../../components/collections-overview/CollectionsOverview";
+import Collection from "../collection/Collection";
 
-const ShopPage = () => {
+const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
-      <CollectionsOverview />
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={Collection} />
     </div>
   );
 };
