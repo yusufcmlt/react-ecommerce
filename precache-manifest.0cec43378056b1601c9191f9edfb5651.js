185f685d0a22d339628a60db7f59e1321d779c4d
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-10-16 12:52:50 +0300

    Updates

---
+++ precache-manifest.0cec43378056b1601c9191f9edfb5651.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62efb97f6e436393371f4f823bc478e9",
+    "url": "/react-ecommerce/index.html"
+  },
+  {
+    "revision": "241baf10bd136236dd72",
+    "url": "/react-ecommerce/static/css/main.b6a7c7ee.chunk.css"
+  },
+  {
+    "revision": "0b4fb17545c89b8db965",
+    "url": "/react-ecommerce/static/js/2.7eb25cc4.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/react-ecommerce/static/js/2.7eb25cc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "241baf10bd136236dd72",
+    "url": "/react-ecommerce/static/js/main.6fba2ee2.chunk.js"
+  },
+  {
+    "revision": "86ccc4ec7485bfaf9c7a",
+    "url": "/react-ecommerce/static/js/runtime-main.ea96c490.js"
+  },
+  {
+    "revision": "f536f3de685b10853f394758a689c569",
+    "url": "/react-ecommerce/static/media/shopping-bag.f536f3de.svg"
+  },
+  {
+    "revision": "c1047b0c3fde97ffcd49d3b820cc84d3",
+    "url": "/react-ecommerce/static/media/sport-wear-colored.c1047b0c.png"
+  },
+  {
+    "revision": "5f29d90c0a5dc418477b0088adc4c36e",
+    "url": "/react-ecommerce/static/media/sport-wear.5f29d90c.png"
+  }
+]);
