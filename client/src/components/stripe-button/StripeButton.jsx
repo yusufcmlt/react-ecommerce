5a59aaf83550e56d83bf5d5d6f4c90c5a3da8690
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-10-20 19:28:03 +0300

    add backend and seperate client-server projects

--- client/src/components/stripe-button/StripeButton.jsx
+++ client/src/components/stripe-button/StripeButton.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -8,8 +9,22 @@ const StripeButton = ({ price }) => {
     "pk_test_51Hc6tIJ8KN2gJsBHWOfHDD3A83fjat1uYb9e6oe7GeRBqHB4RGLQPP8DS3k8O2tOHbckE3WsX0mU1LtZzIbkYWqE00JZNZfEYq";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      //only works with exact url (not with path only)
+      url: "http://localhost:5000/payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment Successfull");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Payment Failed. Please use the provided credit card.");
+      });
   };
 
   return (
