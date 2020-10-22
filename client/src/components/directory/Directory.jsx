2be7f7be44d45ea2b6d2bcde3d472d9dcaf7553d
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-10-22 19:52:42 +0300

    changes for mobile design

--- client/src/components/directory/Directory.jsx
+++ client/src/components/directory/Directory.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { selectDirectorySection } from "../../redux/directory/directory-selectors";
-import Billboard from "../homepage-billboard/Billboard";
 
 import MenuItem from "../menu-item/MenuItem";
 import "./Directory-style.scss";
@@ -10,7 +9,6 @@ import "./Directory-style.scss";
 const Directory = ({ sections }) => {
   return (
     <div className="directory-page">
-      <Billboard />
       <h1>COLLECTIONS</h1>
       <div className="directory-menu">
         {sections.map(({ id, ...otherSectionProps }) => {
--- client/src/pages/homepage/Homepage.jsx
+++ client/src/pages/homepage/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Directory from "../../components/directory/Directory";
+import Billboard from "../../components/homepage-billboard/Billboard";
 
 import "./HomePage-style.scss";
 const Homepage = () => {
   return (
     <div className="homepage">
+      <Billboard />
       <Directory />
     </div>
   );
