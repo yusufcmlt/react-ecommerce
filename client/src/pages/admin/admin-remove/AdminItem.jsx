255029babb40ef679a1a50cfb1e883d2ed955f04
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-10-27 20:55:03 +0300

    changed item remove interactions

--- client/src/pages/admin/admin-remove/AdminItem.jsx
+++ client/src/pages/admin/admin-remove/AdminItem.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import { updateItemToCollection } from "../../../firebase/firebase-utils";
 
 const AdminItem = ({ imageUrl, name, price, category, id, fetchFunc }) => {
-  const removeItem = async (categoryId, item) => {
-    try {
-      await updateItemToCollection(categoryId, item, "remove");
-      alert(`${item.name} removed`);
-      fetchFunc();
-    } catch (error) {
-      alert("Error Removing Item Check Console");
-      console.log(error);
+  const removeItem = async (categoryId, { id, imageUrl, name, price }) => {
+    if (window.confirm(`Do you really want to remove item: ${name}`)) {
+      try {
+        await updateItemToCollection(
+          categoryId,
+          { id, imageUrl, name, price: price.toString() },
+          "remove"
+        );
+        alert(`${name} removed`);
+        fetchFunc();
+      } catch (error) {
+        alert("Error Removing Item Error,Check Console ");
+        console.log(error);
+      }
     }
   };
 
