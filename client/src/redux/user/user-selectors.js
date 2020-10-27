b05a232fd01700c7e43b0ec485ab72657f409aae
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-10-27 20:55:50 +0300

    added user admin selector

--- client/src/redux/user/user-selectors.js
+++ client/src/redux/user/user-selectors.js
@@ -6,3 +6,8 @@ export const selectCurrentUser = createSelector(
   [selectUser],
   (user) => user.currentUser
 );
+
+export const selectIsUserAdmin = createSelector(
+  [selectCurrentUser],
+  (currentUser) => (currentUser ? currentUser.isAdmin : false)
+);
